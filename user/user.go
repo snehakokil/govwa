43276f6a3f14e278e4e22f7a2ebdf7ec6eb69708
Paroksh Sharma <parokshs@synopsys.com>
2019-08-05 22:51:58 -0400

    final

--- user/user.go
+++ user/user.go
@@ -91,8 +91,6 @@ func loginAction(w http.ResponseWriter, r *http.Request, _ httprouter.Params) bo
 
 	uData := checkUserQuery(uname, pass) //handle user data from db
 	if uData.cnt == 1 {
-		fmt.Printf("I am authenticated")
-		fmt.Println("FOO:", os.Getenv("SESSION_SECRET"))
 		s := session.New()
 
 		/* save user data to session */
@@ -105,7 +103,6 @@ func loginAction(w http.ResponseWriter, r *http.Request, _ httprouter.Params) bo
 		log.Println("Login Success")
 		return true
 	} else {
-		fmt.Printf("I am not authenticated")
 		return false
 	}
 }
